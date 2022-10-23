a4a1c246bfe3ea8631937c5e7afd21abd55943be
Esau Guerra <eguerra@mitsoftware.com>
2022-10-23 19:00:01 -0400

    adde

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div className='flex w-full content-center 
+    justify-center bg-greenLengs text-black border-black border-2
+    h-20
+    '>
+        <div className='w-1/5 h-full text-center border-r-black border-r-2 grid place-items-center'>#</div>
+        <div className="w-1/2 h-full border-r-black border-r-2"></div>
+        <div className='w-1/4 h-full text-center grid place-items-center'
+            onClick={()=>{
+                console.log("CONECTANDO")
+            }}
+        >CONECT METAMASK</div>  
+    </div>
+  )
+}
---
+++ components/Principal/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full  text-black'>
+      <div className='grid grid-rows-3'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+      </div>
+    </div>
+  )
+}
+
+export default About
