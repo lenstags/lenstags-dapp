7e96ff1353d637620a9cf00e96776b76a4dbb0c6
CryptoNahue <87866711+cryptonahue@users.noreply.github.com>
2022-11-22 22:42:30 -0300

    feat: import designs from other lenstags project (#3)
    * feat: import designs from other lenstags project
    
    * ♻️ refactor: delete unecesary files
    
    Co-authored-by: Lucas Vallejo <lucasvallejo92@gmail.com>

---
+++ __mocks__/profileMockData.ts
@@ -0,0 +1,4 @@
+export const profileMockData = {
+  userName: 'User',
+  profilePic: 'https://media.lenster.xyz/tr:n-avatar,tr:di-placeholder.webp/https://lens.infura-ipfs.io/ipfs/bafkreidxilha4jw7w3r2ksgzdy4vlshiefi6e3zaymd7z65223iscibfwu'
+}
