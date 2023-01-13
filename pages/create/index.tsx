81ee72924e0ed5e411fc051d23724e320008b9e4
LeoSagan <leo@bloq.com>
2023-01-13 18:30:23 +0000

    Adds gassless txs

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -2,29 +2,45 @@ import { Layout } from 'components';
 import { ProfileContext } from 'components';
 import { NextPage } from 'next';
 import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
-import Image from 'next/image';
 import Editor from 'components/Editor';
 import { createPost, postData } from '@lib/lens/post';
+import { createPostGasless } from '@lib/lens/post-gasless';
+import { queryProfile } from '@lib/lens/dispatcher';
 
 const Create: NextPage = () => {
   const [name, setName] = useState('');
   const [title, setTitle] = useState('');
+  const [dispatcherStatus, setDispatcherStatus] = useState<boolean | undefined>(
+    undefined
+  );
+
   const [abstract, setAbstract] = useState('');
   const [editorContents, setEditorContents] = useState('');
   const [link, setLink] = useState('');
   const [cover, setCover] = useState('');
   const [tags, setTags] = useState([]);
   // const [post, setPost] = useState<postData | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const lensProfile = useContext(ProfileContext);
+  if (!lensProfile) {
+    return null;
+  }
+
+  queryProfile({ profileId: lensProfile.id }).then((profile) => {
+    setDispatcherStatus(
+      profile?.dispatcher?.canUseRelay ? profile.dispatcher.canUseRelay : false
+    );
+
+    return;
+  });
+
   const initialContent = 'Write something nice and styled!';
 
   const handleChangeEditor = (content: string) => {
     setEditorContents(content);
     console.log('Content>>> ', content);
   };
-
-  const [loading, setLoading] = useState(false);
 
   const handlePost = async () => {
     console.log({ title, abstract, editorContents, link });
@@ -47,7 +63,11 @@ const Create: NextPage = () => {
       if (!lensProfile) {
         return;
       }
-      const pubId = await createPost(lensProfile.id, constructedPost);
+
+      const pubId = dispatcherStatus
+        ? await createPostGasless(lensProfile.id, constructedPost)
+        : await createPost(lensProfile.id, constructedPost);
+
       console.log('pubId', pubId);
     } catch (e: any) {
       console.error(e);
@@ -55,12 +75,35 @@ const Create: NextPage = () => {
     setLoading(false);
   };
 
-  // useEffect(() => {}, [onSubmit]);
-
   return (
-    <Layout title="Lenstags | Settings" pageDescription="Settings">
+    <Layout title="Lenstags | Create post" pageDescription="Create post">
       <div className="container mx-auto py-10 h-64 md:w-1/2 w-11/12 px-6 text-black">
-        <p className="text-xl font-semibold">New post</p>
+        <div className="text-xl font-semibold">
+          <span className="text-left">Create post</span>
+          <div
+            className="tooltip tooltip-bottom float-right"
+            data-tip="By enabling gasless transactions you will able to sign once and post
+          for free!"
+          >
+            <button
+              data-tooltip-target="tooltip-default"
+              className={
+                dispatcherStatus === undefined
+                  ? 'bg-stone-200 rounded-lg px-2 py-2 text-xs'
+                  : dispatcherStatus
+                  ? 'bg-green-300 rounded-lg px-2 py-1 text-xs'
+                  : 'bg-red-300 rounded-lg px-2 py-1  text-xs'
+              }
+            >
+              Gasless tx are{' '}
+              {dispatcherStatus === undefined
+                ? 'loading...'
+                : dispatcherStatus
+                ? 'Enabled'
+                : 'Disabled'}
+            </button>
+          </div>
+        </div>
 
         <div className="px-6 py-1 my-4 flex place-items-baseline border rounded border-gray-300 shadow-md ">
           <p className=" ">TITLE</p>
@@ -71,10 +114,6 @@ const Create: NextPage = () => {
             id="title"
             onChange={(e) => {
               setTitle(e.target.value);
-              // console.log("Tit1 ", e.target.value);
-              // console.log("Tit2 ", e);
-              // console.log("Tit3 ", title);
-
               return;
             }}
           />
