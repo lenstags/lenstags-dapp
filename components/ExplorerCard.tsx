a90b11fe2349b26f5650b9bd639879ae10fb3b41
CryptoNahue <87866711+cryptonahue@users.noreply.github.com>
2023-01-17 19:19:38 +0000

    feat: cards changes

--- components/ExplorerCard.tsx
+++ components/ExplorerCard.tsx
@@ -9,16 +9,16 @@ interface Props {
 
 const ExploreCard: FC<Props> = ({ post }) => {
   return (
-    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 ">
+
+{/* animate-in slide-in-from-bottom duration-1000 */}
       <article
-        style={{ backgroundColor: '#F9F9F9' }}
-        className="overflow-hidden rounded-lg shadow-lg
-        animate-in slide-in-from-bottom duration-1000
-        "
+        className="Lens_CardsDiv"
       >
+        <div className="bg-lensGray Lens_Cards rounded-lg">
         <header className="w-full items-center p-2 md:p-4">
-          <div className="flex row w-full text-sm font-light text-black">
-            <div className="col-span-3 mr-2">
+          <div className="flex row justify-between w-full text-sm font-light text-black">
+            <div className="col-span-3 mr-2 flex justify-between">
               {
                 <ImageProxied
                   category="profile"
@@ -31,15 +31,20 @@ const ExploreCard: FC<Props> = ({ post }) => {
                   src={post.profile.picture?.original?.url}
                 />
               }
-            </div>
-            <div className="col-span-1">
+               <div className="col-span-1 pl-2">
               <p className=" ">{post.profile.name || post.profile.id}</p>
               <p className="text-gray-400">@{post.profile.handle}</p>
             </div>
+            </div>
+           
             <div className="col-span-1 text-right ">
-              <span className=" text-xs text-gray-400 text-right">
-                {moment(post.createdAt).format('MMM Do YY')}
-              </span>
+            <ImageProxied
+                      category="profile"
+                      src="/assets/icons/dots-vertical.svg"
+                      alt="Lenstags Logo"
+                      width={20}
+                      height={20}
+                    />
             </div>
             {/* <div className="col-span-1 text-right">icons</div> */}
           </div>
@@ -55,19 +60,16 @@ const ExploreCard: FC<Props> = ({ post }) => {
             src={post.metadata.media[0]?.original.url}
           />
 
-          <div className="mb-2 text-right">
+          <div className="mb-2 flex justify-between px-2">
+            asdasd
             <span
-              className="bg-gray-100 text-green-800 text-xs font-light mr-2 
-          px-2.5 py-0.5 rounded "
+              className="bg-gray-100 text-green-800 text-xs font-light  
+           py-0.5 rounded "
             >
-              {post.profile.stats?.totalCollects || '0'} Collected
+              {moment(post.createdAt).format('MMM Do YY')}
+              
             </span>
-            <button
-              className="bg-lime-200 text-green-800 text-xs font-light mr-2 
-          px-2.5 py-0.5 rounded-lg hover:bg-lime-300"
-            >
-              Collect
-            </button>
+           
           </div>
 
           <div className="px-3 text-sm font-light">
@@ -98,9 +100,17 @@ const ExploreCard: FC<Props> = ({ post }) => {
             <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
           </svg>
           <span className="text-xs">
+            
             {post.profile.stats?.totalComments || '0'}
+
+            <button
+              className="bg-lime-200 text-green-800 text-xs font-light mr-2 
+          px-2.5 py-0.5 rounded-lg hover:bg-lime-300"
+            >
+              Collect
+            </button>
           </span>
-        </footer>
+        </footer></div>
       </article>
     </div>
   );
