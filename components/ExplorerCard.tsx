2714d44ec64f23ba6ec688a560540ae09f6607b4
LeoSagan <raiseerco@gmail.com>
2023-01-23 18:22:01 -0300

    Open a post on a new tab, with all details (#58)
    * Adds the big post on a new tab
    
    * Fix linting

--- components/ExplorerCard.tsx
+++ components/ExplorerCard.tsx
@@ -1,20 +1,23 @@
 import React, { FC } from 'react';
 import ImageProxied from './ImageProxied';
 import moment from 'moment';
-import { profile } from 'console';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface Props {
   post: any;
 }
 
 const ExploreCard: FC<Props> = ({ post }) => {
+  const router = useRouter();
   return (
-    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3  ">
+    <div className="my-1 w-full px-1 md:w-1/2 lg:my-4 lg:w-1/3 lg:px-4  ">
       {/* animate-in slide-in-from-bottom duration-1000 */}
+
       <article className="Lens_CardsDiv">
-        <div className="bg-white Lens_Cards rounded-lg">
+        <div className="Lens_Cards rounded-lg bg-white">
           <header className="w-full items-center p-2 md:p-4">
-            <div className="flex row justify-between w-full text-sm font-light text-black">
+            <div className="row flex w-full justify-between text-sm font-light text-black">
               <div className="col-span-3 mr-2 flex justify-between">
                 {
                   <ImageProxied
@@ -24,27 +27,20 @@ const ExploreCard: FC<Props> = ({ post }) => {
                     height={50}
                     width={50}
                     objectFit="cover"
-                    className="cursor-pointer rounded-full w-12 h-12"
+                    className="h-12 w-12 cursor-pointer rounded-full"
                     src={post.profile.picture?.original?.url}
                   />
                 }
-                <div className="col-span-1 pl-2 cursor-pointer">
+
+                <div className="col-span-1 cursor-pointer pl-2">
                   <p className=" ">{post.profile.name || post.profile.id}</p>
                   <p className="text-gray-400">@{post.profile.handle}</p>
                 </div>
               </div>
 
               <div className=" cursor-pointer ">
-                {/* <ImageProxied
-                      category="profile"
-                      src="/assets/icons/dots-vertical.svg"
-                      alt="Lenstags Logo"
-                      width={20}
-                      height={20}
-                    /> */}
-
-                <div className="dropdown inline-block relative">
-                  <div className=" text-gray-700 font-semibold py-2  rounded  items-center">
+                <div className="dropdown relative inline-block">
+                  <div className=" items-center rounded py-2  font-semibold  text-gray-700">
                     <span className="">
                       <ImageProxied
                         category="profile"
@@ -55,10 +51,10 @@ const ExploreCard: FC<Props> = ({ post }) => {
                       />
                     </span>
                   </div>
-                  <ul className="dropdown-menu right-1 z-10 absolute hidden text-lensBlack  rounded-lg border-2 border-lensBlack ">
+                  <ul className="dropdown-menu absolute right-1 z-10 hidden rounded-lg  border-2 border-lensBlack text-lensBlack ">
                     <li className="">
                       <a
-                        className="hover:text-lensGray2 rounded-t-lg bg-lensGray hover:bg-lensGray3 py-2 px-6 block whitespace-no-wrap"
+                        className="whitespace-no-wrap block rounded-t-lg bg-lensGray py-2 px-6 hover:bg-lensGray3 hover:text-lensGray2"
                         href="#"
                       >
                         Share
@@ -66,7 +62,7 @@ const ExploreCard: FC<Props> = ({ post }) => {
                     </li>
                     <li className="">
                       <a
-                        className="hover:text-lensGray2 rounded-b-lg bg-lensGray hover:bg-lensGray3 py-2 px-6 block whitespace-no-wrap"
+                        className="whitespace-no-wrap block rounded-b-lg bg-lensGray py-2 px-6 hover:bg-lensGray3 hover:text-lensGray2"
                         href="#"
                       >
                         Report
@@ -80,44 +76,57 @@ const ExploreCard: FC<Props> = ({ post }) => {
           </header>
 
           <div>
-            <ImageProxied
-              category="post"
-              height={400}
-              width={600}
-              objectFit="cover"
-              className="block h-auto w-full"
-              src={post.metadata.media[0]?.original.url}
-            />
+            <Link href={`/posts/${post.id}`}>
+              <a
+                target="_blank"
+                onClick={() => {
+                  window.localStorage.setItem(
+                    'LENS_POST',
+                    JSON.stringify(post)
+                  );
+                }}
+              >
+                <ImageProxied
+                  category="post"
+                  height={400}
+                  width={600}
+                  objectFit="cover"
+                  className="block h-auto w-full"
+                  src={post.metadata.media[0]?.original.url}
+                />
+
+                <p className=" text-xl font-semibold text-lensBlack">
+                  {post.metadata.title}
+                  <div
+                    className=" text-base font-normal "
+                    dangerouslySetInnerHTML={{ __html: post.metadata.content }}
+                  ></div>
+                </p>
+              </a>
+            </Link>
           </div>
+          <br></br>
           <div className="mb-2 flex justify-between">
             <div
-              className="bg-lensGray3 border-2 border-lensBlack  text-xs font-light mr-2 
-          px-2 py-0.5 rounded-lg"
+              className="mr-2 rounded-lg border-2 border-lensBlack bg-lensGray3 px-2 
+          py-0.5 text-xs font-light"
             >
               12 Collected
             </div>
             <div
-                className="bg-lensGray border-2 border-lensBlack px-2 text-xs font-light  
-           py-0.5 rounded-lg"
-              >
-                {moment(post.createdAt).format('MMM Do YY')}
-              </div>
-          </div>
-          <div>
-            <a className=" text-xl font-semibold text-lensBlack" href="#">
-              {post.metadata.title} Title post jeje
-              <div className=" text-base font-normal "
-                dangerouslySetInnerHTML={{ __html: post.metadata.content }}
-              ></div>
-            </a>
+              className="rounded-lg border-2 border-lensBlack bg-lensGray px-2 py-0.5  
+           text-xs font-light"
+            >
+              {moment(post.createdAt).format('MMM Do YY')}
+            </div>
           </div>
 
           <div>
-            <ul className=" py-2 flex flex-wrap text-xs gap-1">
+            <ul className=" flex flex-wrap gap-1 py-2 text-xs">
               {post.metadata.tags.map((tag: string) => (
                 <li
                   key={tag}
-                  className="rounded-lg border-2 bg-lensGreen font-semibold border-lensBlack px-2 "
+                  className="rounded-lg border-2 border-lensBlack bg-lensGreen px-2 font-semibold "
                 >
                   {tag}
                 </li>
@@ -125,8 +134,8 @@ const ExploreCard: FC<Props> = ({ post }) => {
             </ul>
           </div>
 
-          <footer className="flex justify-between  items-center text-right text-black px-4 py-2">
-            <span className="text-xs flex items-center ">
+          <footer className="flex items-center  justify-between px-4 py-2 text-right text-black">
+            <span className="flex items-center text-xs ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-messages"
@@ -148,8 +157,8 @@ const ExploreCard: FC<Props> = ({ post }) => {
             </span>
 
             <button
-              className="border-2 border-solid border-lensBlack  bg-lensPurple text-lensGray   text-xs font-light mr-2 
-          px-2.5 py-0.5 rounded-lg hover:bg-lensGray2"
+              className="mr-2 rounded-lg border-2  border-solid border-lensBlack   bg-lensPurple px-2.5 py-0.5 
+          text-xs font-light text-lensGray hover:bg-lensGray2"
             >
               Collect
             </button>
