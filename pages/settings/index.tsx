803cdabb6de04aeb543b8fb2a76c66e9306e2b9b
Fedev <76790873+fedeloterstein@users.noreply.github.com>
2023-01-25 12:42:53 -0300

    fix bug (#63)

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -71,7 +71,7 @@ const router = useRouter()
                     fill="currentFill"
                   />
                 </svg>}
-                <button disabled={loading} onClick={handleChange} className={`${checked ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700'} disabled:bg-gray-300 text-white font-bold py-2 px-4 rounded-full `}>{checked ? 'Disable dispatcher' : 'Enable dispatcher'}</button>
+                <button disabled={loading} onClick={handleChange} className={`${checked ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700'} disabled:bg-gray-300 text-white font-bold py-2 px-4 rounded-full `}>{checked ? 'Disable' : 'Enable'}</button>
 
               </div>
 
