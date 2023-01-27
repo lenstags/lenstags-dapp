7e87d75996016c2160ba7593fdaf3e0178ea0a88
LeoSagan <leo@bloq.com>
2023-01-27 14:38:53 +0000

    Landing fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -261,9 +261,9 @@ const Home: NextPage = () => {
                 className="mr-2 h-6 w-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
               LISTS
@@ -356,7 +356,10 @@ const Home: NextPage = () => {
             </Link>
           </div>
 
+          <hr className="mx-14 my-28  border-2 border-solid border-lensGreen bg-lensGreen  shadow-md shadow-lensBlack" />
+
           {/* team */}
+          <div className="my-10 text-4xl">TEAM</div>
           <div className="mx-28 flex justify-center gap-8 rounded-lg border-4 border-solid border-lensGreen p-8">
             <div>
               <p className="mb-2 text-8xl">😎</p>
@@ -392,27 +395,33 @@ const Home: NextPage = () => {
           </div>
 
           {/* team */}
-          <div className="mx-28 mt-20 flex justify-center gap-8   p-8">
-            <div className="flex gap-14 text-left text-2xl text-purple-500">
+          <div className="mx-28 mt-20 flex justify-center gap-8 p-8">
+            <div className="flex gap-20 text-left text-2xl leading-loose -tracking-tighter text-purple-500">
               <div className=" ">
-                <h1>LEARN MORE</h1>
-                <p>About</p>
-                <p>Documentation</p>
-                <p>LensTags Institutional</p>
-                <p>Security</p>
+                <h1 className="text-3xl">LEARN MORE</h1>
+                <div className="font-thin text-black">
+                  <p>About</p>
+                  <p>Documentation</p>
+                  <p>LensTags Institutional</p>
+                  <p>Security</p>
+                </div>
               </div>
 
               <div>
-                <h1>CONNECT</h1>
-                <p>FAQs</p>
-                <p>Support</p>
-                <p>News</p>
-                <p>Twitter</p>
+                <h1 className="text-3xl">CONNECT</h1>
+                <div className="font-thin text-black">
+                  <p>FAQs</p>
+                  <p>Support</p>
+                  <p>News</p>
+                  <p>Twitter</p>
+                </div>
               </div>
 
               <div>
-                <h1>LEGAL</h1>
-                <p>Privacy</p>
+                <h1 className="text-3xl">LEGAL</h1>
+                <div className="font-thin text-black">
+                  <p>Privacy</p>
+                </div>
               </div>
             </div>
           </div>
