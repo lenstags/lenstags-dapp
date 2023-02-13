76239b16f427ee63d03330b1bed81d90e621918d
fedeloterstein <federicoloterstein@gmail.com>
2023-02-13 20:14:54 -0300

    refactor profiles

--- pages/profiles/[id].tsx
+++ pages/profiles/[id].tsx
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import moment from 'moment';
 import ImageProxied from 'components/ImageProxied';
+import { Layout } from 'components';
+import { Avatar, Button, Container, Divider, HStack, Image, Stack, Text } from '@chakra-ui/react';
+import { getIPFSImage } from '@lib/helpers';
 
 export default function PostDetails() {
   const router = useRouter();
@@ -12,60 +15,62 @@ export default function PostDetails() {
     const myObject = JSON.parse(storedObject);
     setProfile(myObject);
   }, [id]);
+  console.log(profile);
 
   return (
-    profile && (
-      <div className="m-4">
-        <article className=" rounded-lg border-2 border-solid border-black bg-white p-4">
-          <header className="w-full items-center p-2 md:p-4">
-            <div className="row flex w-full justify-between text-sm font-light text-black">
-              <div className="col-span-3 mr-2 flex justify-between">
-                {
-                  <ImageProxied
-                    category="profile"
-                    title={`Loading...`}
-                    alt="Profile"
-                    height={50}
-                    width={50}
-                    objectFit="cover"
-                    className="h-12 w-12 cursor-pointer rounded-full"
-                    src={profile.picture?.original?.url}
-                  />
-                }
-                <div className="col-span-1 cursor-pointer pl-2">
-                  <p className=" ">{profile.name || profile.id}</p>
-                  <p className="text-gray-400">@{profile.handle}</p>
-                </div>
-              </div>
-            </div>
-          </header>
-          <section>
-            <div className=" cursor-pointer ">
-              <p>name: {profile.name}</p>
-              <p>bio: {profile.bio}</p>
-              <p>isFollowedByMe: {profile.isFollowedByMe}</p>
-              <p>isFollowing: {profile.isFollowing}</p>
-              <p>followNftAddress: {profile.followNftAddress}</p>
-              <p>picture: {profile.picture?.original?.url}</p>
-              <p>cover: {/* {profile.coverPicture} */}</p>
-              <p>ownedBy: {profile.ownedBy}</p>
-              <p>totalFollowers:{profile.stats?.totalFollowers}</p>
-              <p>totalFollowing: {profile.stats?.totalFollowing}</p>
-              <p>totalMirrors: {profile.stats?.totalMirrors}</p>
-              <p>totalPublications: {profile.stats?.totalPublications}</p>
-              <p>totalCollects: {profile.stats?.totalCollects}</p>
-            </div>
-          </section>
-          <footer className="flex items-center  justify-between px-4 py-2 text-right text-black">
-            {/* <button
-              className="mr-2 rounded-lg border-2  border-solid border-lensBlack   bg-lensPurple px-2.5 py-0.5 
-          text-xs font-light text-lensGray hover:bg-lensGray2"
-            >
-              Collect
-            </button> */}
-          </footer>
-        </article>
-      </div>
-    )
+    <Layout title="Lenstags | Profile" pageDescription="Profile" screen={true}>
+      {profile && (
+        <Stack width={'100%'} height={'100vh'} bg={'#F5F5F5'}>
+          {/**Header Profile */}
+          <Stack bg={'#FFFFFF'} pb={'42px'}>
+          <ImageProxied
+            category="post"
+            height={217}
+            width={600}
+            objectFit="cover"
+    
+            src={profile.coverPicture?.original?.url}
+          />
+          <Image w={'170px'} h={'170px'} borderRadius={'50%'} src={getIPFSImage(profile.picture?.original?.url)} position={'absolute'} left={'15%'} top={'17%'}/>
+          <Stack p={'0px 15%'} width={'100%'}>
+            <HStack justifyContent={'flex-end'} w={'100%'}>
+            <Button bg={'#DEF702'} w={'224px'} h={'46px'}>FOLLOW</Button>
+            </HStack>
+            <Stack pt={'50px'}>
+              <Text fontWeight={500} fontSize={'31px'}>{profile.name}</Text>
+              <Text fontWeight={300} fontSize={'24px'}>@{profile.handle}</Text>
+              <Text fontWeight={400} fontSize={'24px'}>{profile.bio}</Text>
+              <HStack gap={4} pt={'24px'}>
+                <Stack>
+                  <Text fontWeight={400} fontSize={'16px'} color={'#868585'}>Followings</Text>
+                  <Text fontWeight={600} fontSize={'16px'}>{profile.stats?.totalFollowing}</Text>
+                </Stack>
+                <Stack>
+                  <Text fontWeight={400} fontSize={'16px'} color={'#868585'}>Followers</Text>
+                  <Text fontWeight={600} fontSize={'16px'}>{profile.stats?.totalFollowers}</Text>
+                </Stack>
+                <Divider orientation='vertical' />
+                <Stack>
+                  <Text fontWeight={400} fontSize={'16px'} color={'#868585'}>Curated by</Text>
+                  <HStack>
+                    <Avatar size={'xs'}/>
+                    <Avatar size={'xs'}/>
+                    <Avatar size={'xs'}/>
+                  </HStack>
+                </Stack>
+              </HStack>
+            </Stack>
+          </Stack>
+          </Stack>
+          <Stack  p={'0px 15%'} >
+          {/**Feature Card Container */}
+          <Stack p={'33px'} w={'100%'} h={'360px'} bg={'#FFFFFF'} borderRadius={'16px'}>
+
+          </Stack>
+          </Stack>
+        </Stack>
+      )
+      }
+    </Layout>
   );
 }
