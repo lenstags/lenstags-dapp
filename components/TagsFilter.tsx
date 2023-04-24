2feb909f19241e3eab68824cdb1f478b773417f8
Leo Sagan <raiseerco@gmail.com>
2023-04-24 19:28:56 -0300

    Rebases it all

--- components/TagsFilter.tsx
+++ components/TagsFilter.tsx
@@ -8,7 +8,6 @@ const selectedStyle = 'bg-lensGreen text-black border-black shadow';
 
 // TODO Improve UI when there are too many tags for two lines
 export const TagsFilter = () => {
-  const [selected, setSelected] = useState<boolean[]>([]);
   const { setTags } = useContext(TagsFilterContext);
 
   const [selectedOption, setSelectedOption] = useState([]);
@@ -18,37 +17,8 @@ export const TagsFilter = () => {
     setTags(selectedOptions.map((t: any) => t.value));
   };
 
-  const toggleSelected = (index: number) => {
-    const newSelected = [...selected];
-    newSelected[index] = !selected[index];
-    setSelected(newSelected);
-
-    const tags = TAGS.map((tag) => tag.value).filter(
-      (_, index) => newSelected[index]
-    );
-    setTags(tags);
-  };
-
   return (
     <div className="lens-input z-20 my-4 flex ">
-      {/* <div className="flex justify-center">
-        <div className=" my-4 flex max-h-16 max-w-7xl flex-wrap justify-center gap-2 overflow-hidden text-xs">
-          {TAGS.map((tag, index) => (
-            <span
-              className={`${
-                selected[index]
-                  ? selectedStyle
-                  : 'rounded-lg border-2 border-lensBlack bg-gray-100 font-normal hover:bg-lensGreen hover:font-semibold'
-              } flex flex-1 cursor-pointer select-none  justify-center whitespace-nowrap rounded-lg border-2 px-2 py-1 font-semibold`}
-              key={index}
-              onClick={() => toggleSelected(index)}
-            >
-              {tag.label}
-            </span>
-          ))}
-        </div>
-      </div> */}
-
       <span className="ml-4 font-semibold">Tags</span>
       <div className="w-full border-0 pl-4 ">
         <CreatableSelect
