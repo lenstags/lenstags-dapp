314cda02286e7898cf9d9daafb461830b7577297
LeoSagan <raiseerco@gmail.com>
2023-05-13 17:50:39 -0300

    New landing page :)'

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,13 @@ module.exports = {
       {
         mytheme: {
           primary: '#DEF702',
-
           secondary: '#D926A9',
-
           accent: '#1FB2A6',
-
           neutral: '#191D24',
-
           'base-100': '#2A303C',
-
           info: '#3ABFF8',
-
           success: '#36D399',
-
           warning: '#FBBD23',
-
           error: '#F87272'
         }
       }
@@ -345,11 +337,10 @@ module.exports = {
     fontFamily: {
       sans: [
         'Inter',
-        'NeutralFace',
-        'NeutrafaceText-Light',
-        'NeutrafaceText-Bold',
-        'NeutrafaceText-Demi',
-        'NeutrafaceText-Book',
+        'Inter-Regular',
+        'Inter-Semibold',
+        'Inter-Medium',
+        'Inter-Black',
         'ui-sans-serif',
         'system-ui',
         '-apple-system',
@@ -365,14 +356,7 @@ module.exports = {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"'
       ],
-      serif: [
-        'ui-serif',
-        'Georgia',
-        'Cambria',
-        '"Times New Roman"',
-        'Times',
-        'serif'
-      ],
+      serif: ['SpaceGrotesk', '"Times New Roman"'],
       mono: [
         'ui-monospace',
         'SFMono-Regular',
