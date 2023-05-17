49c0e1ccf7d726bf789e7fd971e08cbe3baad074
LeoSagan <raiseerco@gmail.com>
2023-05-16 21:56:47 -0300

    Draft design of unauthorized screen

--- components/UnauthorizedScreen.tsx
+++ components/UnauthorizedScreen.tsx
@@ -4,17 +4,20 @@ export const UnauthorizedScreen = () => {
   const { openConnectModal } = useConnectModal();
 
   return (
-    <div className="hero flex min-h-screen items-center justify-center bg-black">
+    <div className="hero flex min-h-screen items-center justify-center ">
       <div className="hero-overlay  bg-opacity-60"></div>
       <div className="hero-content  text-neutral-content text-center">
-        <div className="max-w-md ">
-          <h1 className="mb-5 text-5xl font-bold text-white">Welcome to</h1>
-          <img src="img/logo-extended-white.svg"></img>
-          <p className="mb-5 text-lensGray">Please sign in with your wallet</p>
+        <div className="flex max-w-md flex-col justify-center text-center ">
+          <p className=" font-sans text-2xl font-bold">Welcome to</p>
+          <img className="my-8" src="img/landing/nata-logo.svg" />
+          <p className=" my-4 mb-5 font-sans text-gray-400">
+            Please sign in with your wallet
+          </p>
           <div className="flex justify-center">
             <button
               onClick={openConnectModal}
-              className="btn hover:bg-LensGreen flex items-center gap-1 rounded-lg border-none bg-lensGreen px-6  py-2 text-black"
+              className="flex items-center gap-1 rounded-full border-none bg-teal-200
+               px-6 py-2 font-serif text-black"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +35,7 @@ export const UnauthorizedScreen = () => {
                 <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
                 <path d="M20 12h-13l3 -3m0 6l-3 -3" />
               </svg>
-              Sign In
+              Sign in
             </button>
           </div>
         </div>
