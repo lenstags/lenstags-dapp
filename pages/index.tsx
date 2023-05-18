14b5b52ebf2530a9de8502e92712651cc73cfecb
LeoSagan <raiseerco@gmail.com>
2023-05-18 03:23:52 -0300

    Adds OGL shaders

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,19 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import React, { useEffect, useRef, useState } from 'react';
+import {
+  Camera,
+  Flowmap,
+  Geometry,
+  Mesh,
+  Program,
+  Renderer,
+  Texture,
+  Triangle,
+  Vec2,
+  Vec4
+} from 'ogl-typescript';
+import React, { useEffect, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
 
@@ -14,6 +26,42 @@ import type { NextPage } from 'next';
 SwiperCore.use([Autoplay]);
 
 const Home: NextPage = () => {
+  const imgSize = [1440, 680];
+
+  const vertex = `
+					attribute vec2 uv;
+					attribute vec2 position;
+					varying vec2 vUv;
+					void main() {
+							vUv = uv;
+							gl_Position = vec4(position, 0, 1);
+					}
+			`;
+  const fragment = `
+					precision highp float;
+					precision highp int;
+					uniform sampler2D tWater;
+					uniform sampler2D tFlow;
+					uniform float uTime;
+					varying vec2 vUv;
+					uniform vec4 res;
+
+					void main() {
+
+							// R and G values are velocity in the x and y direction
+							// B value is the velocity length
+							vec3 flow = texture2D(tFlow, vUv).rgb;
+
+							vec2 uv = .5 * gl_FragCoord.xy / res.xy ;
+							vec2 myUV = (uv - vec2(0.5))*res.zw + vec2(0.5);
+							myUV -= flow.xy * (0.15 * 0.7);
+
+							vec3 tex = texture2D(tWater, myUV).rgb;
+
+							gl_FragColor = vec4(tex.r, tex.g, tex.b, 1.0);
+					}
+			`;
+
   const items = [
     {
       name: 'TAGS',
@@ -61,6 +109,178 @@ const Home: NextPage = () => {
     }
   ];
 
+  const canvasRef = useRef<HTMLDivElement | null>(null); // used to avoid Property 'appendChild' does not exist on type 'never'
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const renderer = new Renderer({ dpr: 2 });
+    const gl = renderer.gl;
+    const camera = new Camera(gl);
+    let canvas = gl.canvas;
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.zIndex = '-10';
+    canvasRef.current.appendChild(canvas);
+
+    let aspect = 1;
+    const mouse = new Vec2(-1);
+    const velocity = new Vec2();
+
+    function resize() {
+      let a1, a2;
+      var imageAspect = imgSize[1] / imgSize[0];
+      if (window.innerHeight / window.innerWidth < imageAspect) {
+        a1 = 1;
+        a2 = window.innerHeight / window.innerWidth / imageAspect;
+      } else {
+        a1 = (window.innerWidth / window.innerHeight) * imageAspect;
+        a2 = 1;
+      }
+      mesh.program.uniforms.res.value = new Vec4(
+        window.innerWidth,
+        window.innerHeight,
+        a1,
+        a2
+      );
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      aspect = window.innerWidth / window.innerHeight;
+    }
+
+    const flowmap = new Flowmap(gl);
+    // Triangle that includes -1 to 1 range for 'position', and 0 to 1 range for 'uv'.
+    const geometry = new Geometry(gl, {
+      position: {
+        size: 2,
+        data: new Float32Array([-1, -1, 3, -1, -1, 3])
+      },
+      uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) }
+    });
+    const texture = new Texture(gl, {
+      minFilter: gl.LINEAR,
+      magFilter: gl.LINEAR
+    });
+    const imgSrc = 'img/landing/hero.jpg';
+    const img = imgRef.current;
+    if (img) {
+      img.onload = () => {
+        texture.image = img;
+      };
+      img.src = imgSrc;
+    }
+
+    let a1, a2;
+    var imageAspect = imgSize[1] / imgSize[0];
+    if (window.innerHeight / window.innerWidth < imageAspect) {
+      a1 = 1;
+      a2 = window.innerHeight / window.innerWidth / imageAspect;
+    } else {
+      a1 = (window.innerWidth / window.innerHeight) * imageAspect;
+      a2 = 1;
+    }
+
+    const program = new Program(gl, {
+      vertex,
+      fragment,
+      uniforms: {
+        uTime: { value: 0 },
+        tWater: { value: texture },
+        res: {
+          value: new Vec4(window.innerWidth, window.innerHeight, a1, a2)
+        },
+        img: { value: new Vec2(imgSize[0], imgSize[1]) },
+        // Note that the uniform is applied without using an object and value property
+        // This is because the class alternates this texture between two render targets
+        // and updates the value property after each render.
+        tFlow: flowmap.uniform
+      }
+    });
+    const mesh = new Mesh(gl, { geometry, program });
+
+    window.addEventListener('resize', resize, false);
+    resize();
+
+    // Create handlers to get mouse position and velocity
+    const isTouchCapable = 'ontouchstart' in window;
+    if (isTouchCapable) {
+      window.addEventListener('touchstart', updateMouse, false);
+      window.addEventListener('touchmove', updateMouse, { passive: false });
+    } else {
+      window.addEventListener('mousemove', updateMouse, false);
+    }
+    let lastTime: any;
+    const lastMouse = new Vec2();
+
+    let velocityNeedsUpdate = false;
+
+    function updateMouse(e: any) {
+      e.preventDefault();
+      if (e.changedTouches && e.changedTouches.length) {
+        e.x = e.changedTouches[0].pageX;
+        e.y = e.changedTouches[0].pageY;
+      }
+      if (e.x === undefined) {
+        e.x = e.pageX;
+        e.y = e.pageY;
+      }
+      // Get mouse value in 0 to 1 range, with y flipped
+      mouse.set(e.x / gl.renderer.width, 1.0 - e.y / gl.renderer.height);
+      // Calculate velocity
+      if (!lastTime) {
+        // First frame
+        lastTime = performance.now();
+        lastMouse.set(e.x, e.y);
+      }
+
+      const deltaX = e.x - lastMouse.x;
+      const deltaY = e.y - lastMouse.y;
+
+      lastMouse.set(e.x, e.y);
+
+      let time = performance.now();
+
+      // Avoid dividing by 0
+      let delta = Math.max(10.4, time - lastTime);
+      lastTime = time;
+      velocity.x = deltaX / delta;
+      velocity.y = deltaY / delta;
+      // Flag update to prevent hanging velocity values when not moving
+      velocityNeedsUpdate = true;
+    }
+
+    requestAnimationFrame(update);
+
+    function update(t: any) {
+      requestAnimationFrame(update);
+      // Reset velocity when mouse not moving
+      if (!velocityNeedsUpdate) {
+        mouse.set(-1);
+        velocity.set(0);
+      }
+      velocityNeedsUpdate = false;
+      // Update flowmap inputs
+      flowmap.aspect = aspect;
+      flowmap.mouse.copy(mouse);
+      // Ease velocity input, slower when fading out
+      flowmap.velocity.lerp(velocity, velocity.len() ? 0.15 : 0.1);
+      flowmap.update();
+      program.uniforms.uTime.value = t * 0.01;
+      renderer.render({ scene: mesh });
+    }
+
+    return () => {
+      window.removeEventListener('resize', resize);
+      window.removeEventListener('mousemove', updateMouse);
+      window.removeEventListener('touchstart', updateMouse);
+      window.removeEventListener('touchmove', updateMouse);
+    };
+  }, []);
+
   return (
     <div className="">
       <Head>
@@ -72,16 +292,26 @@ const Home: NextPage = () => {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
+
+      {/* <div className="bg-white" ref={canvasRef} /> */}
+
       <LayoutLanding title={'Nata Social | Home'} pageDescription={'Home'}>
         <div>
           <div
-            className="-mt-20 w-full py-6 md:pb-28 md:pt-48"
-            style={{
-              background: 'url(/img/landing/nata-back-hero.svg)',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat'
-            }}
+            ref={canvasRef}
+            className="relative -mt-20 w-full py-6 md:pb-28 md:pt-48"
+            // style={{
+            //   background: 'url(/img/landing/nata-back-hero.svg)',
+            //   backgroundSize: 'cover',
+            //   backgroundRepeat: 'no-repeat'
+            // }}
           >
+            <img
+              className=" hidden "
+              ref={imgRef}
+              src="img/landing/hero.jpg"
+              alt="Background"
+            />
             <div
               className=" mx-6 my-8 mt-16 rounded-3xl border-2 border-gray-100 bg-white bg-opacity-70 
              py-12 text-center align-middle 
