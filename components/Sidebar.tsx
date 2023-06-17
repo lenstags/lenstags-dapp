3078ca9fdc8e3841fb119623b0c7717010889077
LeoSagan <raiseerco@gmail.com>
2023-06-17 12:21:00 -0300

    Arranges sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -43,7 +43,7 @@ const Sidebar: React.FC<SidebarProps> = ({ position }) => {
     >
       {position === 'left' ? (
         <>
-          <div className="px-6 pb-2">
+          <div className="px-6 pb-6">
             <Link href={'/'}>
               <Image
                 className=" cursor-pointer  "
@@ -308,107 +308,14 @@ const Sidebar: React.FC<SidebarProps> = ({ position }) => {
                       </div>
                     </div>
                   </div>
-
-                  // <ul className=" absolute rounded border-r bg-white font-extralight text-black shadow">
-                  // <li className="flex w-full cursor-pointer justify-between border-b px-5 py-3 ">
-                  //   <div className="fl first-letter:ex">
-                  //     <Link
-                  //       href={'/my-profile'}
-                  //       className="ml-2 hover:font-bold "
-                  //     >
-                  //       <p>
-                  //         <p className="text-xs font-thin text-stone-400">
-                  //           Connected as
-                  //         </p>
-                  //         <span className=" text-green-500">@</span>
-                  //         <span className=" text-green-700">
-                  //           {lensProfile?.handle}
-                  //         </span>
-                  //       </p>
-                  //     </Link>
-                  //   </div>
-                  // </li>
-                  // <li className="items-left flex w-full cursor-pointer border-b px-5 py-3">
-                  //   <div className="float-left flex items-center text-left">
-                  //     <svg
-                  //       xmlns="http://www.w3.org/2000/svg"
-                  //       className="icon icon-tabler icon-tabler-user mr-2 hover:text-red-600"
-                  //       width={18}
-                  //       height={18}
-                  //       viewBox="0 0 24 24"
-                  //       strokeWidth="1.5"
-                  //       stroke="currentColor"
-                  //       fill="none"
-                  //       strokeLinecap="round"
-                  //       strokeLinejoin="round"
-                  //     >
-                  //       <path stroke="none" d="M0 0h24v24H0z" />
-                  //       <circle cx={12} cy={7} r={4} />
-                  //       <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                  //     </svg>
-                  //     <Link
-                  //       href={'/my-profile'}
-                  //       className="ml-2 hover:font-bold "
-                  //     >
-                  //       My profile
-                  //     </Link>
-                  //   </div>
-                  // </li>
-                  // <li className="items-left flex w-full cursor-pointer border-b px-5 py-3">
-                  //   <div className="float-left flex items-center text-left">
-                  //     <svg
-                  //       xmlns="http://www.w3.org/2000/svg"
-                  //       viewBox="0 0 24 24"
-                  //       fill="currentColor"
-                  //       className="mr-1 h-6 w-6"
-                  //     >
-                  //       <path
-                  //         fillRule="evenodd"
-                  //         d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
-                  //         clipRule="evenodd"
-                  //       />
-                  //     </svg>
-
-                  //     <Link href={'/settings'} className=" hover:font-bold ">
-                  //       Settings
-                  //     </Link>
-                  //   </div>
-                  // </li>
-                  // <li className="flex w-full cursor-pointer items-center justify-between border-b px-5 py-3 hover:text-red-600">
-                  //   <div className="flex items-center">
-                  //     <svg
-                  //       xmlns="http://www.w3.org/2000/svg"
-                  //       className="icon icon-tabler icon-tabler-logout"
-                  //       width={20}
-                  //       height={20}
-                  //       viewBox="0 0 24 24"
-                  //       strokeWidth="1.5"
-                  //       stroke="currentColor"
-                  //       fill="none"
-                  //       strokeLinecap="round"
-                  //       strokeLinejoin="round"
-                  //     >
-                  //       <path stroke="none" d="M0 0h24v24H0z" />
-                  //       <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-                  //       <path d="M7 12h14l-3 -3m0 6l3 -3" />
-                  //     </svg>
-                  //     <span
-                  //       className="ml-2 hover:text-red-600"
-                  //       onClick={handleDisconnect}
-                  //     >
-                  //       Disconnect
-                  //     </span>
-                  //   </div>
-                  // </li>
-                  // </ul>
                 )}
               </>
             ) : (
               <button
                 onClick={openConnectModal}
                 className="  rounded-lg 
-          p-2 align-middle font-serif font-medium tracking-wider text-white
-          "
+                p-2 align-middle font-serif font-medium tracking-wider text-white
+                "
               >
                 ✦ CONNECT
               </button>
@@ -416,7 +323,7 @@ const Sidebar: React.FC<SidebarProps> = ({ position }) => {
           </div>
 
           {/* top creators */}
-          <div className="rounded-t-lg bg-stone-200 py-4 pr-4">
+          <div className="mt-2 rounded-t-lg bg-stone-200 py-4 pr-4">
             <div className=" rounded-lg pl-4">
               <div className=" flex items-baseline justify-between">
                 <p className=" font-serif text-sm font-bold">Top Creators</p>
