4337f49bd75de44c8a92a0f26be68d4b256f1ee0
LeoSagan <raiseerco@gmail.com>
2023-07-03 18:31:29 -0300

    Adds more curated tags (#158)

--- lib/lens/tags.ts
+++ lib/lens/tags.ts
@@ -5,135 +5,29 @@ export type Tag = {
 };
 
 export const TAGS: Tag[] = [
-  {
-    color: '#ddf500',
-    value: 'web3',
-    label: 'Web3'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'layer2',
-    label: 'Layer 2'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'governance',
-    label: 'Governance'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'product-management',
-    label: 'Product Management'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'ux-ui',
-    label: 'UX/UI'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'food',
-    label: 'Food'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'design-systems',
-    label: 'Design Systems'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'tokenomics',
-    label: 'Tokenomics'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'solidity',
-    label: 'Solidity'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'proof-of-stake',
-    label: 'Proof of Stake'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'movies-series',
-    label: 'Movies & Series'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'play-to-earn',
-    label: 'Play 2 Earn'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'decentralization',
-    label: 'Decentralization'
-  },
-  {
-    color: '#ddf500',
-
-    value: '3d-design',
-    label: '3d Design'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'nft',
-    label: 'NFT'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'lending',
-    label: 'Lending'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'communities',
-    label: 'Communities'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'art',
-    label: 'art'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'metaverse',
-    label: 'Metaverse'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'esports',
-    label: 'eSports'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'dao',
-    label: 'DAO'
-  },
-  {
-    color: '#ddf500',
-
-    value: 'comics',
-    label: 'Comics'
-  }
+  { color: '#ddf555', value: 'ai-ml', label: 'AI-ML' },
+  { color: '#ddf555', value: 'bitcoin', label: 'Bitcoin' },
+  { color: '#ddf555', value: 'daos', label: 'DAOs' },
+  { color: '#ddf555', value: 'defi', label: 'DeFi' },
+  { color: '#ddf555', value: 'design', label: 'Design' },
+  { color: '#ddf555', value: 'education', label: 'Education' },
+  { color: '#ddf555', value: 'entertainment', label: 'Entertainment' },
+  { color: '#ddf555', value: 'ethereum', label: 'Ethereum' },
+  { color: '#ddf555', value: 'food', label: 'Food' },
+  { color: '#ddf555', value: 'gaming', label: 'Gaming' },
+  { color: '#ddf555', value: 'governance', label: 'Governance' },
+  { color: '#ddf555', value: 'health', label: 'Health' },
+  { color: '#ddf555', value: 'home', label: 'Home' },
+  { color: '#ddf555', value: 'latam', label: 'LATAM' },
+  { color: '#ddf555', value: 'layer 2', label: 'Layer 2' },
+  { color: '#ddf555', value: 'metaverse', label: 'Metaverse' },
+  { color: '#ddf555', value: 'nature', label: 'Nature' },
+  { color: '#ddf555', value: 'nft', label: 'NFT' },
+  { color: '#ddf555', value: 'other', label: 'Other' },
+  { color: '#ddf555', value: 'privacy', label: 'Privacy' },
+  { color: '#ddf555', value: 'refi', label: 'ReFi' },
+  { color: '#ddf555', value: 'scaling', label: 'Scaling' },
+  { color: '#ddf555', value: 'science', label: 'Science' },
+  { color: '#ddf555', value: 'social', label: 'Social' },
+  { color: '#ddf555', value: 'startup', label: 'Startup' }
 ];
