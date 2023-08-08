c4fab466db26dfc29e8cc25d803698759e89dd09
LeoSagan <raiseerco@gmail.com>
2023-08-07 21:33:40 -0300

    Fixes broadcast on dispatcher (#211)

--- lib/lens/enable-dispatcher.ts
+++ lib/lens/enable-dispatcher.ts
@@ -12,62 +12,11 @@ import {
 } from './ethers.service';
 
 import { apolloClient } from '@lib/lens/graphql/apollo-client';
-import { gql } from '@apollo/client/core';
+import { broadcastRequest } from './broadcast';
 import { lensHub } from '@lib/lens/lens-hub';
+import { pollUntilIndexed } from './graphql/has-transaction-been-indexed';
 
-const CREATE_SET_DISPATCHER_TYPED_DATA = gql`
-  mutation CreateSetDispatcherTypedData(
-    $options: TypedDataOptions
-    $request: SetDispatcherRequest!
-  ) {
-    createSetDispatcherTypedData(options: $options, request: $request) {
-      id
-      expiresAt
-      typedData {
-        types {
-          SetDispatcherWithSig {
-            name
-            type
-            __typename
-          }
-          __typename
-        }
-        domain {
-          name
-          chainId
-          version
-          verifyingContract
-          __typename
-        }
-        value {
-          nonce
-          deadline
-          profileId
-          dispatcher
-          __typename
-        }
-        __typename
-      }
-      __typename
-    }
-  }
-`;
-
-export const enableDispatcherWithTypedData = async (request: {
-  profileId: string;
-  enable: boolean;
-}) => {
-  const result = await apolloClient.mutate({
-    mutation: CREATE_SET_DISPATCHER_TYPED_DATA,
-    variables: {
-      request
-    }
-  });
-
-  return result.data!.createSetDispatcherTypedData;
-};
-
-export const enableDispatcherWithTypedDataold = async (
+export const enableDispatcherWithTypedData = async (
   request: SetDispatcherRequest
 ) => {
   const result = await apolloClient.mutate({
@@ -79,32 +28,41 @@ export const enableDispatcherWithTypedDataold = async (
   return result.data!.createSetDispatcherTypedData;
 };
 
-export const enableDispatcher = async (profileId: string) => {
-  try {
-    if (!profileId) {
-      throw new Error('Must define PROFILE_ID in the .env to run this');
-    }
+export const enable = async (profileId: any) => {
+  if (!profileId) {
+    throw new Error('Must define profileId');
+  }
 
-    const address = await getAddressFromSigner();
-    console.log('address got: ', address);
-    const result = await enableDispatcherWithTypedData({
-      profileId,
-      enable: true
-    });
+  const address = await getAddressFromSigner();
+  const result = await enableDispatcherWithTypedData({
+    profileId,
+    enable: true
+    // leave it blank if you want to use the lens API dispatcher!
+    // dispatcher: '0xEEA0C1f5ab0159dba749Dc0BAee462E5e293daaF',
+  });
+  console.log('set dispatcher: enableDispatcherWithTypedData', result);
 
-    const typedData = result.typedData;
-    console.log('set dispatcher: typedData', typedData);
+  const typedData = result.typedData;
+  console.log('set dispatcher: typedData', typedData);
 
-    const signature = await signedTypeData(
-      typedData.domain,
-      typedData.types as any,
-      typedData.value
-    );
-    console.log('set dispatcher: signature', signature);
+  const signature = await signedTypeData(
+    typedData.domain,
+    typedData.types as any,
+    typedData.value
+  );
+  console.log('set dispatcher: signature', signature);
 
-    if (!signature) {
-      throw new Error('Error splitting signature');
-    }
+  // here
+  const dataBroadcast = await broadcastRequest({
+    id: result.id,
+    signature
+  });
+
+  if (!signature) {
+    throw new Error('Error splitting signature');
+  }
+
+  if (dataBroadcast.__typename === 'RelayError') {
     const { v, r, s } = splitSignature(signature);
 
     const tx = await lensHub.setDispatcherWithSig({
@@ -117,11 +75,74 @@ export const enableDispatcher = async (profileId: string) => {
         deadline: typedData.value.deadline
       }
     });
-    console.log('set dispatcher: tx hash', tx.hash);
-    return true;
-  } catch (err) {
-    console.log('err dispatcher ', err);
-    return false;
+    console.log('set dispatcher: tx hash: ', tx.hash);
+    const indexedResult = await pollUntilIndexed(tx.hash);
+    console.log('indexed! ', indexedResult);
+  }
+};
+
+const disableDispatcherWithTypedData = async (
+  request: SetDispatcherRequest
+) => {
+  const result = await apolloClient.mutate({
+    mutation: CreateSetDispatcherTypedDataDocument,
+    variables: {
+      request
+    }
+  });
+
+  return result.data!.createSetDispatcherTypedData;
+};
+
+export const disable = async (profileId: any) => {
+  if (!profileId) {
+    throw new Error('Must define PROFILE_ID in the .env to run this');
+  }
+
+  const address = getAddressFromSigner();
+  console.log('disable dispatcher: address', address);
+
+  //   await login(address);
+
+  const result = await disableDispatcherWithTypedData({
+    profileId,
+    enable: false
+  });
+  console.log('disable dispatcher: disableDispatcherWithTypedData', result);
+
+  const typedData = result.typedData;
+  console.log('disable dispatcher: typedData', typedData);
+
+  const signature = await signedTypeData(
+    typedData.domain,
+    typedData.types as any,
+    typedData.value
+  );
+  console.log('disable dispatcher: signature', signature);
+
+  const dataBroadcast = await broadcastRequest({
+    id: result.id,
+    signature
+  });
+
+  if (!signature) {
+    throw new Error('Error splitting signature');
+  }
+  console.log('disable dispatcher: dataBroadcast', dataBroadcast);
+  if (dataBroadcast.__typename === 'RelayError') {
+    const { v, r, s } = splitSignature(signature);
+
+    const tx = await lensHub.setDispatcherWithSig({
+      profileId: typedData.value.profileId,
+      dispatcher: typedData.value.dispatcher,
+      sig: {
+        v,
+        r,
+        s,
+        deadline: typedData.value.deadline
+      }
+    });
+    console.log('disable dispatcher: tx hash', tx.hash);
   }
 };
 
