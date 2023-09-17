b8ae94cb55344b608a35bb6aa3f1cdfb720eed27
LeoSagan <raiseerco@gmail.com>
2023-09-17 01:06:49 -0300

    Updates explore profiles (#337)

---
+++ lib/lens/explore-profiles.ts
@@ -0,0 +1,35 @@
+import { apolloClient } from './graphql/apollo-client';
+import {
+  ExploreProfilesDocument,
+  ExploreProfilesRequest,
+  ProfileSortCriteria
+} from './graphql/generated';
+
+// sort out types by generating them!
+const exploreProfiles = async (request: ExploreProfilesRequest) => {
+  const result = await apolloClient.query({
+    query: ExploreProfilesDocument,
+    variables: {
+      request
+    }
+  });
+
+  return result.data.exploreProfiles;
+};
+
+// CreatedOn = 'CREATED_ON',
+// LatestCreated = 'LATEST_CREATED',
+// MostCollects = 'MOST_COLLECTS',
+// MostComments = 'MOST_COMMENTS',
+// MostFollowers = 'MOST_FOLLOWERS',
+// MostMirrors = 'MOST_MIRRORS',
+// MostPosts = 'MOST_POSTS',
+// MostPublication = 'MOST_PUBLICATION'
+
+export const getExploreProfiles = async () => {
+  const result = await exploreProfiles({
+    sortCriteria: ProfileSortCriteria.MostFollowers
+  });
+
+  return result;
+};
